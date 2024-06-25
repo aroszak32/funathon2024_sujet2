5f157fe3517a8c1ae1f104a2a18bc3b05491739e
aroszak <adeline.roszak@agriculture.gouv.fr>
2024-06-25 09:31:24 +0000

    Exercice 1

--- R/create_data_list.R
+++ R/create_data_list.R
@@ -1,6 +1,15 @@
+#### Exercice 1 ####
+
 library(yaml)
 
 create_data_list <- function(source_file){
   catalogue <- yaml::read_yaml(source_file)
   return(catalogue)
 }
+
+urls <- create_data_list("R/sources.yml")
+getwd()
+
+source("correction/R/create_data_list.R")
+
+
