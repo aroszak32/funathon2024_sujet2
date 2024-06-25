fcea4a4d072d29225407ca735bffd85a0b0bc479
aroszak <adeline.roszak@agriculture.gouv.fr>
2024-06-25 09:50:46 +0000

    Exercice import des données

---
+++ R/import_data.R
@@ -0,0 +1,65 @@
+## Exercice Import ##
+
+##Import des données aéroports##
+import_airport_data <- function(list_files){
+  
+  pax_apt_all <- readr::read_csv2(
+    list_files, 
+    col_types = cols(
+      ANMOIS = col_character(),
+      APT = col_character(),
+      APT_NOM = col_character(),
+      APT_ZON = col_character(),
+      .default = col_double()
+    )
+  ) %>% 
+    clean_dataframe()
+  
+  return(pax_apt_all)
+  
+}
+
+source("R/clean_dataframe.R")
+
+##Import des données compagnies##
+import_compagnies_data <- function(list_files){
+  
+  pax_cie_all <- readr::read_csv2(
+    file = list_files,
+    col_types = cols(
+      ANMOIS = col_character(),
+      CIE = col_character(),
+      CIE_NOM = col_character(),
+      CIE_NAT = col_character(),
+      CIE_PAYS = col_character(),
+      .default = col_double()
+    )
+  ) %>% 
+    clean_dataframe()
+  
+  return(pax_cie_all)
+  
+  
+}
+
+## Import des données liaisons ##
+import_liaisons_data <- function(list_files){
+  
+  pax_lsn_all <- readr::read_csv2(
+    file = list_files,
+    col_types = cols(
+      ANMOIS = col_character(),
+      LSN = col_character(),
+      LSN_DEP_NOM = col_character(),
+      LSN_ARR_NOM = col_character(),
+      LSN_SCT = col_character(),
+      LSN_FSC = col_character(),
+      .default = col_double()
+    ) 
+  ) %>% 
+    clean_dataframe()
+  
+  return(pax_lsn_all)
+  
+  
+}
