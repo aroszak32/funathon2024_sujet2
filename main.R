b273ad844118b09292350d029ada0ca778840d0d
aroszak <adeline.roszak@agriculture.gouv.fr>
2024-06-25 14:36:14 +0000

    graphique dynamique

--- main.R
+++ main.R
@@ -10,6 +10,7 @@ source("correction/R/import_data.R")
 source("correction/R/create_data_list.R")
 source("correction/R/clean_dataframe.R")
 source("correction/R/figures.R")
+source("R/figures.R")
 
 
 # Load data ----------------------------------
@@ -31,25 +32,4 @@ trafic <- pax_apt_all %>%
   filter(apt %in% default_airport) %>% 
   mutate(date=as.Date(paste0(anmois,'01'),"%Y%m%d"))
 
-## figure statique
-fig <- plot_ly(trafic, x = ~date, y = ~trafic, type = 'scatter', mode = 'lines')
-fig
 
-figure_plotly <- plot_airport_line(trafic, default_airport)
-
-## graphique dynamique
-graph_dynamique <- function(df, selection_aeroport){
-  trafic <- df %>% 
-    mutate(trafic=apt_pax_dep + apt_pax_tr + apt_pax_arr) %>% 
-    filter(apt %in% default_airport) %>% 
-    mutate(date=as.Date(paste0(anmois,'01'),"%Y%m%d"))
-  
-  fig <- trafic %>% 
-    plot_ly(x = ~date, y = ~trafic, 
-            text = ~apt_nom,
-            type = 'scatter', mode = 'lines+markers')
-  
-  return(fig)
-  
-}
-graph_dynamique(pax_apt_all,"LFAQ")
